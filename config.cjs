ffaf97d6a251f116823ef70ee32a85bdd452e0d4
Mihiruk2006Samurai <136062458+Mihiruk2006Samurai@users.noreply.github.com>
2024-07-15 00:17:47 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&YbcpSAmY",
+  SESSION_ID: process.env.SESSION_ID || "MASTER-MD&xEwkV8z0",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
@@ -19,7 +19,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "ＭＲＭＩＨＩＲＵＫＤＩＬＳＨＡＮ",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94701073037",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94778764083",
   GEMINI_KEY: process.env.GEMINI_KEY || "",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
